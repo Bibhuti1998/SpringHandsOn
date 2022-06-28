f5c4daaefbbce6b1a43bf9579ab755be01b609d7
Bibhuti_Bhusan <panibibhutibhusan2@gmail.com>
2022-06-29 00:43:26 +0530

    Configure jsp instead of themlyfe

--- src/main/java/com/javaNoobDeveloper/SpringHandsOn/Controller/WelcomeController.java
+++ src/main/java/com/javaNoobDeveloper/SpringHandsOn/Controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping("/welcome")
+	@RequestMapping("/")
 	public String welcomePage() {
 		return "welcome";
 	}
